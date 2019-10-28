5504c71474e993c6d36bfe9fddc774c7d4a083fe
manuelanton <s.manuel.anton@gmail.com>
2019-10-28 15:41:01 -0300

    creo que está el store funcionando

--- front/src/index.js
+++ front/src/index.js
@@ -1,12 +1,15 @@
+import { Provider } from "react-redux";
+import store from "./store";
 import React from "react";
 import ReactDOM from "react-dom";
-
 import { BrowserRouter, Route } from "react-router-dom";
 import App from "./components/App";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Route path="/" component={App} />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("app")
 );
--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
-import rootReducer from "./reducers";
+import { rootReducer } from "./reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
